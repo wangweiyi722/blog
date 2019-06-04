83fd38b47bdb60051417f7648f96bae79d4b8b7d
Weiyi Wang <weiyiwang@google.com>
2019-06-04 16:59:46 -0500

    finished app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
 // Action creator
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log('about to fetch posts');
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  console.log(userIds);
+}
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -8,5 +18,22 @@ export const fetchPosts = () => async (dispatch) => {
       payload: response.data
     });
   };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
+  });
+};
+
+//memoized solution:
+// const _fetchUser = _.memoize(async (id,dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// });
 
 // Create a named export. Export many different functions from a single file
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchPosts} from '../actions';
+import UserHeader from './UserHeader';
+import {fetchPostsAndUsers} from '../actions';
 
 class PostList extends React.Component{
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
+
+  // Helper render function
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId}/>
+          </div>
+        </div>
+      );
+    })
+  }
+
   render() {
-    console.log(this.props.posts);
-    return <div>Post List</div>;
+
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
@@ -17,4 +37,4 @@ const mapStateToProps = (state) => {
   return {posts: state.posts};
 };
 
-export default connect(mapStateToProps,{fetchPosts:fetchPosts})(PostList);
+export default connect(mapStateToProps,{fetchPostsAndUsers:fetchPostsAndUsers})(PostList);
